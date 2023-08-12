e5e10aa1903c0894c9d34991631554a088831bcc
Sarda29 <142050170+Sarda29@users.noreply.github.com>
2023-08-12 11:36:44 -0400

    statfile

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terra_state.id
   versioning_configuration {
     status = "Enabled"
   }
